d8badd213957b57531b09fbf7b0e6f2abbb3d2ef
Rafael Silveira Rocha <rafarocha1511@gmail.com>
2025-01-06 18:35:13 -0300

    start: initial commit

---
+++ src/test/java/com/rafael/rocha/spring_challenge/SpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.rocha.spring_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
